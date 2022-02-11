38c0235448889eab7cfc64868969a0b2eea2cd9b
Mukhametrakhimov Rustam <ruskb16@gmail.com>
2022-02-11 14:43:35 +0500

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let str = '';
+    let m = n;
+    if (m < 0) {
+        str = String(-m);
+    } else { 
+        str = String(m);
+    }
+    let str2 = '';
+    let len = str.length;
+    for (let i = len-1; i >= 0; i--) {
+      str2 += str[i];
+    };
+    return Number(str2);
 }
